6f15a3f485c66d3e734dc06afd34a689ea92a89d
seleanridley <ridley.s@husky.neu.edu>
2018-08-13 17:42:58 -0400

    Adding Js and Json directories

--- form/views.py
+++ form/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .form import UserForm
 
 # Create your views here.
@@ -6,8 +7,11 @@ def index(request):
     form = UserForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
-            
+
             pass
         else:
             form = UserForm()
     return render(request, "index.html", {'form': form})
+
+def datavis(request):
+    return render(request, "datavis.html")
--- test_app/urls.py
+++ test_app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from form.views import index
+from form.views import datavis
 
 urlpatterns = [
     path('', index ),
+    path('', datavis ),
     path('admin/', admin.site.urls),
 ]
