35ad1b54a61b24de64b3e96e7038c874f785ef97
Cameron Bates <bates.ca@husky.neu.edu>
2018-08-14 04:08:31 -0400

    Search now prints News, Reddit, and Twitter search on console.

--- form/Controller.py
+++ form/Controller.py
@@ -3,14 +3,15 @@ from . import redditAPI
 from . import TwitterAPI
 import json
 from datetime import date, datetime
+import time
 
 '''
 	Contributors: Chris Lau and Cameron Bates
 '''
 class SearchParam(object):
 	def __init__(self, start_date, end_date, keyword, twitter, reddit):
-		self.startDate = datetime.strptime(start_date, '%d/%m/%Y')
-		self.endDate = datetime.strptime(start_date, '%d/%m/%Y')
+		self.startDate = datetime.strptime(start_date, '%m/%d/%Y').date()
+		self.endDate = datetime.strptime(end_date, '%m/%d/%Y').date()
 		self.keyword = keyword
 		if twitter and not reddit:
 			self.platform = 'Twitter'
@@ -20,22 +21,22 @@ class SearchParam(object):
 			self.platform = None #throw an error!
 
 	def runSearch(self):
-		startDate_str = search_param.startDate.strftime('%Y-%m-%d')
-		endDate_str = search_param.endDate.strftime('%Y-%m-%d')
+		startDate_str = self.startDate.strftime('%Y-%m-%d')
+		endDate_str = self.endDate.strftime('%Y-%m-%d')
 
 		##News API
 		news_api = NewsAPI.NewsAPI()
-		news_api.search(search_param.keyword, startDate_str, endDate_str)
+		news_api.search(self.keyword, startDate_str, endDate_str)
 		news_api.parse_results()
 
 		##Social Media API
-		if search_param.platform = 'Twitter' ##run twitter search
+		if self.platform == 'Twitter': ##run twitter search
 			twitter_api = TwitterAPI.TwitterAPI()
-			twitter_api.search(search_param.keyword, startDate_str, endDate_str)
+			twitter_api.search(self.keyword, startDate_str, endDate_str)
 			twitter_api.parse_results()
 
 
-		elif search_param.platform = 'Reddit' ##run reddit search
+		elif self.platform == 'Reddit': ##run reddit search
 			reddit_api = redditAPI.RedditAPI()
-			reddit_api.search(search_param.keyword, search_param.startDate, search_param.endDate)
+			reddit_api.search(self.keyword, self.startDate, self.endDate)
 			reddit_api.parse_results()
